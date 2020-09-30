4d5ff6fe0ad5650549758a19fa723b41b001ec5a
Alexander <uxanovav@yandex.ru>
2020-09-30 09:32:06 +0400

    feats: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 5;
+exports.min = function min(array) {
+  if (typeof array !== 'object' || array.length === 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    for (let el of array) {
+      if (el < min) {
+        min = el;
+      }
+    }
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (typeof array !== 'object' || array.length === 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (let el of array) {
+      if (el > max) {
+        max = el;
+      }
+    }
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof array !== 'object' || array.length === 0) {
+    return 0;
+  } else {
+    let avg = 0;
+    for (let el of array) {
+      avg += el;
+    }
+    avg = avg / array.length;
+    return avg;
+  }
 }
